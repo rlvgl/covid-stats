113e156444d34003cf6de6aab05ec8cacf0cec08
Sohum Agrawal <78573380+sohumagrawal@users.noreply.github.com>
2021-06-17 21:35:21 -0700

    Update App.jsx
    changed fetch api to axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,43 @@ import AppHeader from './AppHeader';
 import UsCases from './UsCases';
 import InternationalCases from './InternationalCases';
 
+const axios = require('axios');
+
 const App = () => {
     const [page, setPage] = useState('United States');
     const [usData, setUsData] = useState({});
     const [internationalData, setInternationalData] = useState({});
 
-
-    // these both are setting data functions
-    const getUsData = async () => {
-        const response = await fetch('https://disease.sh/v3/covid-19/countries/usa');
-        const jsonResponse = await response.json();
-        setUsData({
-            cases: numberFormatted(jsonResponse.cases),
-            recovered: numberFormatted(jsonResponse.recovered),
-            deaths: numberFormatted(jsonResponse.deaths)
-        })
+    // get request using axios
+    const getUsData = () => {
+        axios.get('https://disease.sh/v3/covid-19/countries/usa')
+            .then(response => {
+                setUsData({
+                cases: numberFormatted(response.cases),
+                recovered: numberFormatted(response.recovered),
+                deaths: numberFormatted(response.deaths)
+                })
+            })
     }
-    
-    const getInternationalData = async () => {
-        const response = await fetch('https://disease.sh/v3/covid-19/all');
-        const jsonResponse = await response.json();
-        setInternationalData({
-            cases: numberFormatted(jsonResponse.cases),
-            recovered: numberFormatted(jsonResponse.recovered),
-            deaths: numberFormatted(jsonResponse.deaths)
-        })
+
+    const getInternationalData = () => {
+        axios.get('https://disease.sh/v3/covid-19/all')
+            .then(response => {
+                setInternationalData({
+                    cases: numberFormatted(response.cases),
+                    recovered: numberFormatted(response.recovered),
+                    deaths: numberFormatted(response.deaths) 
+                })
+            })
     }
 
     // use regex to format numbers
     // straight from stackoverflow
     const numberFormatted = (x) => {
-        x = x.toString();
+        /* x = x.toString();
         var pattern = /(-?\d+)(\d{3})/;
         while (pattern.test(x))
-            x = x.replace(pattern, "$1,$2");
+            x = x.replace(pattern, "$1,$2"); */
         return x;
     }
 
